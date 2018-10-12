e0343bbac6c59948f24e88094c3c5df99f2d5c9c
Arnaud Nauwynck <arnaud.nauwynck@gmail.com>
2018-10-12 08:26:55 +0200

    add test-jpaql-inmem

---
+++ test-jpaql-inmem/src/main/java/fr/an/tests/jpaqlinmem/dto/ADTO.java
@@ -0,0 +1,14 @@
+package fr.an.tests.jpaqlinmem.dto;
+
+import lombok.Data;
+
+@Data
+public class ADTO {
+
+	private int id;
+	
+	private String name;
+
+	private BDTO b;
+	
+}
---
+++ test-jpaql-inmem/src/test/java/fr/an/tests/jpaqlinmem/service/AServiceTest.java
@@ -0,0 +1,48 @@
+package fr.an.tests.jpaqlinmem.service;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import fr.an.tests.jpaqlinmem.domain.AEntity;
+import fr.an.tests.jpaqlinmem.domain.BEntity;
+import fr.an.tests.jpaqlinmem.dto.ADTO;
+import fr.an.tests.jpaqlinmem.repository.ARepo;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AServiceTest {
+
+	BEntity b2 = new BEntity(2, "b2");
+	AEntity a1 = new AEntity(1, "a1", b2); 
+	AEntity a2 = new AEntity(1, "a2", b2); 
+
+	@InjectMocks
+	AService sut = new AService();
+	
+	@Mock
+	ARepo aRepo;
+
+	@Spy
+	DtoConverter converter = new DtoConverter(); 
+	
+	@Test
+	public void testFindByNameLike() {
+		// given
+		Mockito.when(aRepo.findByNameLike("a%")).thenReturn(Arrays.asList(a1, a2));
+		// when
+		List<ADTO> res = sut.findByNameLike("a%");
+		// then
+		Assert.assertEquals(2, res.size());
+		Assert.assertEquals("a1", res.get(0).getName());
+		Assert.assertEquals("a2", res.get(1).getName());
+	}
+
+}
